a96744f37f6d4224cd5b215d155e5414285cf2eb
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-10-29 13:29:39 +0300

    feat(security): add UserRepository, UserDetails, UserDetailsService

---
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/security/UserDetailsServiceImpl.java
@@ -0,0 +1,23 @@
+package com.fitness.tracker.fitness_tracker_api.security;
+
+import com.fitness.tracker.fitness_tracker_api.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String identifier) throws UsernameNotFoundException {
+        return userRepository.findByEmail(identifier)
+                .or(() -> userRepository.findByUsername(identifier))
+                .map(UserDetailsImpl::new)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found: " + identifier));
+    }
+}
