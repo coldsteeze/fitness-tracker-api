705677647027521b20bd85bda2f68a3733d19213
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-10-29 11:57:09 +0300

    feat: add User entity and Liquibase migration for users table

---
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/entity/User.java
@@ -0,0 +1,43 @@
+package com.fitness.tracker.fitness_tracker_api.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class User {
+
+    @Column(name = "id", nullable = false, updatable = false)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "username", unique = true, nullable = false)
+    private String username;
+
+    @Column(name = "email", unique = true, nullable = false)
+    private String email;
+
+    @Column(name = "password", nullable = false)
+    private String password;
+
+    @Column(name = "logged_at")
+    private LocalDateTime loggedAt;
+
+    @Column(name = "created_at", updatable = false, nullable = false)
+    private LocalDateTime createdAt;
+
+    @PrePersist
+    private void prePersist() {
+        this.createdAt = LocalDateTime.now();
+    }
+}
