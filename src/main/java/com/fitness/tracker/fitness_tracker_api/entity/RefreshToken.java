9b4680e635b9217fda6d38fab89607adb8ce8ec0
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-10-30 13:38:07 +0300

    refactor: migrate to Instant for expiresAt and add Builder pattern

--- src/main/java/com/fitness/tracker/fitness_tracker_api/entity/RefreshToken.java
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/entity/RefreshToken.java
@@ -1,12 +1,9 @@
 package com.fitness.tracker.fitness_tracker_api.entity;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Entity
 @Table(name = "refresh_tokens")
@@ -14,6 +11,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @Getter
 @Setter
+@Builder
 public class RefreshToken {
 
     @Id
@@ -28,5 +26,5 @@ public class RefreshToken {
     private String token;
 
     @Column(name = "expires_at", nullable = false)
-    private LocalDateTime expiresAt;
+    private Instant expiresAt;
 }
