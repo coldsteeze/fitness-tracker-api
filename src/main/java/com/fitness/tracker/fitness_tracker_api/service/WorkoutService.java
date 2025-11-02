8a2531f6db5caf0d23e29e0edabe5f32e0fc573c
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-11-02 20:11:42 +0300

    feat(workouts): add WorkoutService interface defining CRUD operations

---
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/service/WorkoutService.java
@@ -0,0 +1,16 @@
+package com.fitness.tracker.fitness_tracker_api.service;
+
+import com.fitness.tracker.fitness_tracker_api.dto.request.WorkoutRequest;
+import com.fitness.tracker.fitness_tracker_api.dto.response.WorkoutResponse;
+import com.fitness.tracker.fitness_tracker_api.entity.User;
+
+public interface WorkoutService {
+
+    WorkoutResponse findById(Long id, User user);
+
+    WorkoutResponse createWorkout(WorkoutRequest workoutRequest, User user);
+
+    WorkoutResponse updateWorkout(Long id, WorkoutRequest workoutRequest, User user);
+
+    void deleteWorkout(Long id, User user);
+}
