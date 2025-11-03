74c219cac34abbaa69ce2f66500cf70536c0cd3c
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-11-03 12:52:45 +0300

    feat(workouts): add WorkoutService interface with findAllWorkouts method

--- src/main/java/com/fitness/tracker/fitness_tracker_api/service/WorkoutService.java
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/service/WorkoutService.java
@@ -1,8 +1,13 @@
 package com.fitness.tracker.fitness_tracker_api.service;
 
 import com.fitness.tracker.fitness_tracker_api.dto.request.WorkoutRequest;
+import com.fitness.tracker.fitness_tracker_api.dto.response.PagedResponse;
 import com.fitness.tracker.fitness_tracker_api.dto.response.WorkoutResponse;
 import com.fitness.tracker.fitness_tracker_api.entity.User;
+import com.fitness.tracker.fitness_tracker_api.entity.enums.WorkoutType;
+import org.springframework.data.domain.Pageable;
+
+import java.time.LocalDate;
 
 public interface WorkoutService {
 
@@ -13,4 +18,13 @@ public interface WorkoutService {
     WorkoutResponse updateWorkout(Long id, WorkoutRequest workoutRequest, User user);
 
     void deleteWorkout(Long id, User user);
+
+    PagedResponse<WorkoutResponse> findAllWorkouts(
+            WorkoutType type,
+            LocalDate dateStart,
+            LocalDate dateEnd,
+            Integer durationStart,
+            Integer durationEnd,
+            Pageable pageable,
+            User user);
 }
