2748cb28e7bb2d6e667525b0a89b223d03136ba5
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-11-04 11:55:14 +0300

    feat(media): add custom exception handling for MediaPhotoService in GlobalExceptionHandler

---
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/exception/media/MediaPhotoAccessDeniedException.java
@@ -0,0 +1,7 @@
+package com.fitness.tracker.fitness_tracker_api.exception.media;
+
+public class MediaPhotoAccessDeniedException extends RuntimeException {
+    public MediaPhotoAccessDeniedException(String message) {
+        super(message);
+    }
+}
