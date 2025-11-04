025cc5c4aa5be1612cf5f61a22c472062f41030e
coldsteeze <160128869+coldsteeze@users.noreply.github.com>
2025-11-04 11:41:15 +0300

    feat(media): add MediaPhotoRepository

---
+++ src/main/java/com/fitness/tracker/fitness_tracker_api/repository/MediaPhotoRepository.java
@@ -0,0 +1,7 @@
+package com.fitness.tracker.fitness_tracker_api.repository;
+
+import com.fitness.tracker.fitness_tracker_api.entity.MediaPhoto;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MediaPhotoRepository extends JpaRepository<MediaPhoto, Long> {
+}
